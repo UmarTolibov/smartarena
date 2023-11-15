ccbb977ac0f3853a1568052f60169f8dbcf834f6
Umar Tolibov <98467609+azimovic1@users.noreply.github.com>
2023-11-16 01:38:26 +0500

    ss

--- test.py
+++ test.py
@@ -3,7 +3,6 @@ from locust import HttpUser, task, between
 
 class MyUser(HttpUser):
     wait_time = between(1, 3)
-    base_url = "https://smartarena-bbff190dd374.herokuapp.com/"
 
     def on_start(self):
         # Perform the initial authentication and obtain a token
@@ -62,6 +61,3 @@ class MyUser(HttpUser):
         print(f"Edit Stadium - Status Code: {response.status_code}, Response: {response.text}")
 
         # You can add more tasks or continue testing other endpoints as needed
-
-
-
